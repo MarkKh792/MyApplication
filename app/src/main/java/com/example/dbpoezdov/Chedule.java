7674897d7fa7890430df91f6ebe992e036b2c5f6
MarkKh792 <ma200@tut.by>
2020-11-26 21:44:45 +0300

    Добавлен просмотр поездов и создание PDF-файла

--- app/src/main/java/com/example/dbpoezdov/Chedule.java
+++ app/src/main/java/com/example/dbpoezdov/Chedule.java
@@ -1,14 +1,124 @@
 package com.example.dbpoezdov;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.graphics.Paint;
+import android.graphics.pdf.PdfDocument;
+import android.os.Environment;
+import android.view.View;
+import android.database.Cursor;
+import android.widget.EditText;
+import android.widget.SimpleCursorAdapter;
+import android.widget.TextView;
+
+import androidx.core.app.ActivityCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
 import android.os.Bundle;
+import android.widget.CalendarView;
+import android.widget.Toast;
+
 
 public class Chedule extends AppCompatActivity {
 
+    RecyclerView recyclerViewChed;
+    //TextView header4;
+    MyDatabaseHelper myDB;
+    TextView ChDay_txt, ChTrain_txt1, ChMarshrut_txt1, ChPribil_txt1, ChYedet_txt1, ChWay_txt1, ChPlatform_txt1;
+
+    ArrayList<String> ChID, ChTrain, ChMarshrut, ChDay, ChPribil, ChYedet, ChWay, ChPlatform;
+
+    CustomAdapter4 customAdapter4;
+    private int mYear;
+    private int mMonth;
+    private int mDay;
+    EditText Day;
+    int IDay;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chedule);
+
+        recyclerViewChed = findViewById(R.id.recyclerViewChed);
+        //header4 = findViewById(R.id.header4);
+
+
+
+        CalendarView calendarView = findViewById(R.id.calendarView);
+        calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+
+            @Override
+            public void onSelectedDayChange(@NonNull CalendarView View, int year, int month, int dayOfMonth) {
+                mYear = year;
+                mMonth = month;
+                mDay = dayOfMonth;
+                String selectedDate = new StringBuilder().append(mMonth + 1)
+                        .append("-").append(mDay).append("-").append(mYear)
+                        .append(" ").toString();
+                Toast.makeText(getApplicationContext(), selectedDate, Toast.LENGTH_LONG).show();
+
+            }
+        });
+
+
+
+
+        myDB = new MyDatabaseHelper(Chedule.this);
+        ChID = new ArrayList<>();
+        ChTrain = new ArrayList<>();
+        //ChDay = new ArrayList<>();
+        ChMarshrut = new ArrayList<>();
+        ChPribil = new ArrayList<>();
+        ChYedet = new ArrayList<>();
+        ChWay = new ArrayList<>();
+        ChPlatform = new ArrayList<>();
+
+        /*Day = findViewById(R.id.ChTextDay);
+        IDay = Integer.valueOf(Day.getText().toString().trim());
+        if (mDay == IDay)
+        {
+
+        }*/
+
+
+        storeDataInArrays();
+
+        customAdapter4 = new CustomAdapter4(Chedule.this,this, ChID, ChTrain, ChMarshrut, ChPribil, ChYedet, ChWay, ChPlatform);
+        recyclerViewChed.setAdapter(customAdapter4);
+        recyclerViewChed.setLayoutManager(new LinearLayoutManager(Chedule.this));
     }
+
+    void storeDataInArrays(){
+        Cursor cursor = myDB.readAllDataCHED();
+        if(cursor.getCount() == 0){
+            //header4.setVisibility(View.VISIBLE);
+        }else{
+            while (cursor.moveToNext()){
+                ChID.add(cursor.getString(0));
+                ChTrain.add(cursor.getString(1));
+                ChMarshrut.add(cursor.getString(2));
+                ChPribil.add(cursor.getString(3));
+                ChYedet.add(cursor.getString(4));
+                ChWay.add(cursor.getString(5));
+                ChPlatform.add(cursor.getString(6));
+            }
+            //header4.setVisibility(View.GONE);
+        }
+    }
+
+
+
 }
--- app/src/main/java/com/example/dbpoezdov/UpdateChed.java
+++ app/src/main/java/com/example/dbpoezdov/UpdateChed.java
@@ -3,19 +3,30 @@ package com.example.dbpoezdov;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
 import android.content.DialogInterface;
+import android.content.pm.PackageManager;
+import android.graphics.Paint;
+import android.graphics.pdf.PdfDocument;
 import android.os.Bundle;
+import android.os.Environment;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import java.io.File;
+import java.io.FileOutputStream;
+
 public class UpdateChed extends AppCompatActivity {
 
     EditText ChTrain_input1, ChMarshrut_input1, ChDay_input1, ChPribil_input1, ChYedet_input1, ChWay_input1, ChPlatform_input1, ChVagonov_input1, ChMest_input1;
     Button UpdateCh_button, DeleteCh_button;
+
+    private EditText text1, text2, text3, text4, text5, text6, text7;
 
     String IDCh, TrainCh, WayCh, DayCh, PribilCh, YedetCh, PutCh, PlatformCh, VagonCh, MestCh;
 
@@ -35,6 +46,16 @@ public class UpdateChed extends AppCompatActivity {
         ChMest_input1 = findViewById(R.id.ChMest_input);
         UpdateCh_button = findViewById(R.id.UpdateCh_button);
         DeleteCh_button = findViewById(R.id.DeleteCh_button);
+
+        ActivityCompat.requestPermissions(UpdateChed.this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE}, PackageManager.PERMISSION_GRANTED);
+
+        text1 = findViewById(R.id.ChTrain_input);
+        text2 = findViewById(R.id.ChMarshrut_input);
+        text3 = findViewById(R.id.ChDay_input);
+        text4 = findViewById(R.id.ChPribil_input);
+        text5 = findViewById(R.id.ChYedet_input);
+        text6 = findViewById(R.id.ChWay_input);
+        text7 = findViewById(R.id.ChPlatform_input);
 
         //First we call this
         getAndSetIntentData();
@@ -103,6 +124,50 @@ public class UpdateChed extends AppCompatActivity {
             Toast.makeText(this, "No data.", Toast.LENGTH_SHORT).show();
         }
     }
+
+    public void createMyPDF(View view){
+
+        PdfDocument myPdfDocument = new PdfDocument();
+        PdfDocument.PageInfo myPageInfo = new PdfDocument.PageInfo.Builder(300,600,1).create();
+        PdfDocument.Page myPage = myPdfDocument.startPage(myPageInfo);
+
+        Paint myPaint = new Paint();
+        String myString = text1.getText().toString();
+        String myString2 = text2.getText().toString();
+        String myString3 = text3.getText().toString();
+        String myString4 = text4.getText().toString();
+        String myString5 = text5.getText().toString();
+        String myString6 = text6.getText().toString();
+        String myString7 = text7.getText().toString();
+
+        int x = 130, y=25;
+        for (String line:myString.split("\n")){
+            myPage.getCanvas().drawText(line, x, y, myPaint);
+            y+=myPaint.descent()-myPaint.ascent();
+        }
+        myPage.getCanvas().drawText("Маршрут: " + myString2,10,50,myPaint);
+        myPage.getCanvas().drawText("День: " + myString3,10,75,myPaint);
+        myPage.getCanvas().drawText("Время прибытия: " + myString4,10,95,myPaint);
+        myPage.getCanvas().drawText("Время отправления: " + myString5,10,115,myPaint);
+        myPage.getCanvas().drawText("Номер пути: " + myString6,10,135,myPaint);
+        myPage.getCanvas().drawText("Номер платформе: " + myString7,10,155,myPaint);
+
+
+        myPdfDocument.finishPage(myPage);
+
+        String myFilePath = Environment.getExternalStorageDirectory().getPath() + "/Download/" + "Расписание.pdf";
+        File myFile = new File(myFilePath);
+        try {
+            myPdfDocument.writeTo(new FileOutputStream(myFile));
+            Toast.makeText(this, "Добавлено в PDF", Toast.LENGTH_SHORT).show();
+        }
+        catch (Exception e){
+
+        }
+
+        myPdfDocument.close();
+    }
+
 
     void confirmDialog(){
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
